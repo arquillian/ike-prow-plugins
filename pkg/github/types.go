df7f743dc3f10dbb44175183f06458032678a406
Bartosz Majsak <bartosz.majsak@gmail.com>
2018-03-20 12:19:03 +0100

    feat(#48): adds repository and sender to logging context (#49)

--- pkg/github/types.go
+++ pkg/github/types.go
@@ -10,6 +10,25 @@ type StatusContext struct {
 }
 
 const (
+	// EventGUID is sent by Github in a header of every webhook request.
+	EventGUID = "event-GUID" // aligned with Prow fields
+	// Event is sent by GitHub in a header of every webhook request.
+	Event = "event-type" // aligned with Prow fields
+	// RepoLogField is the repository from where the event came.
+	RepoLogField = "github-repo"
+	// SenderLogField is the username who caused the event to be sent.
+	SenderLogField = "github-event-sender"
+)
+
+// These are possible State entries for a Status.
+const (
+	StatusPending = "pending"
+	StatusSuccess = "success"
+	StatusError   = "error"
+	StatusFailure = "failure"
+)
+
+const (
 	IssueComment = EventType("issue_comment") // nolint
 	PullRequest  = EventType("pull_request")  // nolint
 )
