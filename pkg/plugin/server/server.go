ed52ac77cbb8608be47ace824fcadbc2ca7a1f65
Bartosz Majsak <bartosz.majsak@gmail.com>
2018-03-20 10:45:30 +0100

    feat(#48): adds repo and sender to logging context

--- pkg/plugin/server/server.go
+++ pkg/plugin/server/server.go
@@ -7,6 +7,8 @@ import (
 	"github.com/arquillian/ike-prow-plugins/pkg/log"
 	"github.com/sirupsen/logrus"
 	"k8s.io/test-infra/prow/hook"
+	gogh "github.com/google/go-github/github"
+	"encoding/json"
 )
 
 // GitHubEventHandler is a type which keeps the logic of handling GitHub events for the given plugin implementation.
@@ -23,6 +25,13 @@ type Server struct {
 	PluginName         string
 }
 
+// repoEvent contains subset of most of the events sent by GitHub such as IssueComment or PullRequest
+// This information is used for contextual logging
+type repoEvent struct {
+	Repo         *gogh.Repository   `json:"repository,omitempty"`
+	Sender       *gogh.User         `json:"sender,omitempty"`
+}
+
 // ServeHTTP validates an incoming webhook and puts it into the event channel.
 func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	// TODO(k8s-prow): Move webhook handling logic out of hook binary so that we don't have to import all
@@ -34,10 +43,20 @@ func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	l := logrus.StandardLogger().WithFields(
 		logrus.Fields{
 			"ike-plugins": s.PluginName,
-			"event-type":  eventType,
-			"event-GUID":  eventGUID,
+			github.EventGUID:  eventGUID,
+			github.Event:  eventType,
 		},
 	)
+
+	var event repoEvent
+	if err := json.Unmarshal(payload, &event); err != nil {
+		l.WithError(err).Warnf("Failed while parsing event with payload: %q.", string(payload))
+	} else {
+		l.WithFields(logrus.Fields{
+			github.RepoLogField: event.Repo.URL,
+			github.SenderLogField: event.Sender.URL,
+		})
+	}
 
 	if err := s.GitHubEventHandler.HandleEvent(l, github.EventType(eventType), payload); err != nil {
 		l.WithError(err).Error("error parsing event.")
