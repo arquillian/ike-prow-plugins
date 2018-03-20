411f24ad9af9b1f209e0c180693bfa727fb914fe
Bartosz Majsak <bartosz.majsak@gmail.com>
2018-03-20 13:09:54 +0100

    docs: introduces Sentry section to documentation (#47)
    * chore: locks raven-go client
    * chore: externalizes logrus and sentry setup to log package
    * docs: adds section about sentry integration
    * fix: makes sentry integration optional

---
+++ pkg/log/sentry.go
@@ -0,0 +1,51 @@
+package log
+
+import (
+	"github.com/sirupsen/logrus"
+	"github.com/evalphobia/logrus_sentry"
+	"time"
+)
+
+// SentryTags encapsulate map of tags where both key and values are strings. Those tags are used by Sentry
+// to categorize events reported by client apps.
+type SentryTags map[string]string
+
+// SentryConfiguration keeps basic configuration used by sentry hook
+type SentryConfiguration struct {
+	Dsn     string
+	Tags    SentryTags
+	Timeout time.Duration
+}
+
+// NewSentryConfiguration creates new instance of SentryConfiguration
+// dsn - full sentry DSN url
+// tags - extra tags to add to every event reported to Sentry
+// timeout - timeout in milliseconds after which connection to Sentry server should be dropped
+func NewSentryConfiguration(dsn string, tags map[string]string, timeout int) SentryConfiguration {
+	return SentryConfiguration{
+		Dsn:     dsn,
+		Tags:    tags,
+		Timeout: time.Duration(timeout) * time.Millisecond,
+	}
+}
+
+// AddSentryHook registers logrus hook integration logger with Sentry
+func AddSentryHook(log *logrus.Entry, configuration SentryConfiguration) {
+	if configuration.Dsn != "" {
+		levels := []logrus.Level{
+			logrus.PanicLevel,
+			logrus.FatalLevel,
+			logrus.ErrorLevel,
+		}
+
+		hook, err := logrus_sentry.NewWithTagsSentryHook(configuration.Dsn, configuration.Tags, levels)
+
+		if err == nil {
+			hook.Timeout = configuration.Timeout
+			logrus.AddHook(hook)
+		} else {
+			log.WithError(err).Error("failed to add sentry hook")
+		}
+	}
+
+}
