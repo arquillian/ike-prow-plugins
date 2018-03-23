022f9991b13305764f7f6fc9333bdbce47f604be
Bartosz Majsak <bartosz.majsak@gmail.com>
2018-03-23 21:07:53 +0100

    chore: minor tweaks in test-keeper PR comments

--- pkg/plugin/test-keeper/plugin/comment_message.go
+++ pkg/plugin/test-keeper/plugin/comment_message.go
@@ -10,25 +10,31 @@ import (
 )
 
 const (
-	beginning = "It seems that there are no tests that would be added or changed in this PR. " +
-		"Automated tests give us confidence in shipping reliable software. Please add some as part of this change.\n" +
-		"If you are an admin and you are sure that no test is needed then you can use command `" + SkipComment + "` " +
+	paragraph = "\n\n"
+
+	beginning = "It appears that no tests have been added or updated in this PR." +
+		paragraph +
+		"Automated tests give us confidence in shipping reliable software. Please add some as part of this change." +
+		paragraph +
+		"If you are an admin and you are sure that no test is needed then you can use the command `" + SkipComment + "` " +
 		"as a comment to make the status green.\n"
 
-	noConfig = "For more information about the plugin and how to configure it, go to the " +
-		"[documentation](http://arquillian.org/ike-prow-plugins/#_test_keeper_plugin)."
+	noConfig = "For more information please head over to official " +
+		"[documentation](http://arquillian.org/ike-prow-plugins/#_test_keeper_plugin). You can find there how to " +
+		"configure the plugin - for example exclude certain file types so if PR contains only them it won't be checked."
 
 	withConfig = "Your plugin configuration is stored in the [file](%s)."
 
-	notFoundFileSuffix = "\nIn the configuration file you pointed to the custom comment file, " +
-		"but the plugin wasn't able to retrieve it from the defined location (%s). Make sure it is either a valid URL or a valid " +
+	notFoundFileSuffix = "In the configuration file you pointed to the custom comment file, " +
+		"but the plugin wasn't able to retrieve it from the defined location (%s). Make sure it is either a valid URL or a " +
 		"path to an existing file in this repository."
+
 )
 
 // CreateCommentMessage creates a comment message for the test-keeper plugin. If the comment message is set in config then it takes that one, the default otherwise.
 func CreateCommentMessage(configuration TestKeeperConfiguration, change scm.RepositoryChange) string {
 	if configuration.LocationURL == "" {
-		return beginning + noConfig
+		return beginning + paragraph + noConfig
 	}
 	if configuration.PluginHint != "" {
 		return getMsgFromFile(configuration, change)
@@ -52,12 +58,12 @@ func getMsgFromFile(configuration TestKeeperConfiguration, change scm.Repository
 	content, err = utils.GetFileFromURL(msgFileURL)
 
 	if err != nil {
-		return getMsgWithConfigRef(configuration.LocationURL) + fmt.Sprintf(notFoundFileSuffix, msgFileURL)
+		return getMsgWithConfigRef(configuration.LocationURL) + paragraph + fmt.Sprintf(notFoundFileSuffix, msgFileURL)
 	}
 
 	return string(content)
 }
 
 func getMsgWithConfigRef(urlToConfig string) string {
-	return beginning + fmt.Sprintf(withConfig, urlToConfig)
+	return beginning + paragraph + fmt.Sprintf(withConfig, urlToConfig)
 }
