7847c7bfb9377d9c94a8fdcc899bd7b6d1d3814f
Hemani <hemanikatyal@gmail.com>
2018-03-26 21:11:23 +0530

    feat(#21): configures JUnit reporter for Ginkgo tests. (#65)

--- pkg/plugin/config/config_suite_test.go
+++ pkg/plugin/config/config_suite_test.go
@@ -3,11 +3,12 @@ package config_test
 import (
 	"testing"
 
+	. "github.com/arquillian/ike-prow-plugins/pkg/internal/test"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
 
 func TestConfig(t *testing.T) {
 	RegisterFailHandler(Fail)
-	RunSpecs(t, "Plugin Config Loader Suite")
+	RunSpecWithJUnitReporter(t, "Plugin Config Loader Suite")
 }
--- pkg/plugin/work-in-progress/plugin/work_in_progress_suite_test.go
+++ pkg/plugin/work-in-progress/plugin/work_in_progress_suite_test.go
@@ -3,11 +3,12 @@ package plugin_test
 import (
 	"testing"
 
+	. "github.com/arquillian/ike-prow-plugins/pkg/internal/test"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
 
 func TestSuiteWorkInProgressPlugin(t *testing.T) {
 	RegisterFailHandler(Fail)
-	RunSpecs(t, "Prow Event Handler Work-in-progress Plugin Suite")
+	RunSpecWithJUnitReporter(t, "Prow Event Handler Work-in-progress Plugin Suite")
 }
