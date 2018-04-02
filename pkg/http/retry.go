0cdbd2072e93033b3da1bf2e3c0e1d964a69f8b5
Matous Jobanek <mjobanek@redhat.com>
2018-04-02 14:25:51 +0200

    extracted retry logic to separated util function

--- pkg/http/retry.go
+++ pkg/http/retry.go
@@ -5,6 +5,7 @@ import (
 	"time"
 	"fmt"
 	"errors"
+	"github.com/arquillian/ike-prow-plugins/pkg/utils"
 )
 
 // RequestInvoker is a function that invokes a request and returns a response
@@ -12,21 +13,21 @@ type RequestInvoker func() (*gogh.Response, error)
 
 // Do invokes a request for the given amount of retries with the given sleep before each one of them until the response status code is lower than 404
 func Do(retries int, sleep time.Duration, invokeRequest RequestInvoker) error {
-	errs := make([]error, 0, retries)
+	errs := utils.Retry(retries, sleep, func() error {
+		response, err := invokeRequest()
 
-	response, err := invokeRequest()
-
-	for i := 0; i < retries-1 && response != nil && response.StatusCode >= 404; i++ {
-		errs = append(errs, err)
-		time.Sleep(sleep)
-		response, err = invokeRequest()
-	}
-
-	if err != nil {
-		errs = append(errs, err)
-		msg := fmt.Sprintf("During %d retries of sending a request %d errs have occurred:", retries, len(errs))
+		if response != nil && response.StatusCode >= 404 {
+			if err != nil {
+				return err
+			}
+			return errors.New("Server responded with error " + string(response.StatusCode))
+		}
+		return nil
+	})
+	if len(errs) > 0 {
+		msg := fmt.Sprintf("During %d retries of sending a request %d errors have occurred:", retries, len(errs))
 		for index, e := range errs {
-			msg = msg + fmt.Sprintf("\n%d. [%s]", index + 1, e.Error())
+			msg = msg + fmt.Sprintf("\n%d. [%s]", index+1, e.Error())
 		}
 		return errors.New(msg)
 	}
