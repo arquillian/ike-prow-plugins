1669fa80b8d78605653edd2977fd4b2c1d1b833e
Bartosz Majsak <bartosz.majsak@gmail.com>
2018-04-16 20:51:42 +0200

    chore: adds tests for rate limit watcher

---
+++ pkg/github/client/rate_limit_watcher_client_test.go
@@ -0,0 +1,70 @@
+package ghclient_test
+
+import (
+	"github.com/arquillian/ike-prow-plugins/pkg/github/client"
+	. "github.com/arquillian/ike-prow-plugins/pkg/internal/test"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"github.com/sirupsen/logrus/hooks/test"
+	"gopkg.in/h2non/gock.v1"
+)
+
+var _ = Describe("GitHub rate limit watcher client", func() {
+
+	logger, hook := test.NewNullLogger()
+
+	client := ghclient.NewRateLimitWatcherClient(NewDefaultGitHubClient(), logger, 10)
+
+	Context("Client should watch rate limits when decorated", func() {
+
+		BeforeEach(func() {
+			defer gock.OffAll()
+			hook.Reset()
+		})
+
+		AfterEach(EnsureGockRequestsHaveBeenMatched)
+
+		It("should not log rate limit when within the threshold", func() {
+			// given
+			gock.New("https://api.github.com").
+				Get("/rate_limit").
+				Persist().
+				Reply(200).
+				Body(FromFile("test_fixtures/gh/high_rate_limit.json"))
+
+			gock.New("https://api.github.com").
+				Get("/repos/owner/repo/pulls/123/files").
+				Reply(200).
+				BodyString("[]")
+
+			// when
+			_, err := client.ListPullRequestFiles("owner", "repo", 123)
+
+			// then
+			Ω(err).ShouldNot(HaveOccurred())
+			Expect(hook.Entries).To(BeEmpty())
+		})
+
+		It("should log rate limit when within the threshold", func() {
+			// given
+			gock.New("https://api.github.com").
+				Get("/rate_limit").
+				Persist().
+				Reply(200).
+				Body(FromFile("test_fixtures/gh/low_rate_limit.json"))
+
+			gock.New("https://api.github.com").
+				Get("/repos/owner/repo/pulls/123/files").
+				Reply(200).
+				BodyString("[]")
+
+			// when
+			_, err := client.ListPullRequestFiles("owner", "repo", 123)
+
+			// then
+			Ω(err).ShouldNot(HaveOccurred())
+			Expect(hook.Entries).To(HaveLen(2))
+			Expect(hook.LastEntry().Message).To(Equal("reaching limit for GH API calls. 8/20 left. resetting at [2018-04-04 20:20:20]"))
+		})
+	})
+})
