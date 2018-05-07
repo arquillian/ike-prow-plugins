648acc34fbdcad531fb995d90635f82d733f3505
Dipak Pawar <dpawar@redhat.com>
2018-05-07 07:35:22 +0530

    chore: add comment for exported type and function

--- pkg/server/metrics.go
+++ pkg/server/metrics.go
@@ -28,12 +28,15 @@ func init() {
 	prometheus.MustRegister(handledEventsCounter)
 }
 
+// Metrics is a set of metrics gathered by the Ike Prow Plugin.
+// It includes rate limit, incoming webhooks. handled events.
 type Metrics struct {
 	RateLimit            *prometheus.GaugeVec
 	WebhookCounter       *prometheus.CounterVec
 	HandledEventsCounter *prometheus.CounterVec
 }
 
+// NewMetrics creates a new set of metrics for the Ike Prow Plugin.
 func NewMetrics() *Metrics {
 	return &Metrics{
 		RateLimit:            rateLimit,
@@ -42,6 +45,7 @@ func NewMetrics() *Metrics {
 	}
 }
 
+// ReportRateLimit reports rate limit for Github API calls.
 func ReportRateLimit(r *github.RateLimits) {
 	rateLimit.WithLabelValues("core").Set(float64(r.Core.Remaining))
 	rateLimit.WithLabelValues("search").Set(float64(r.Search.Remaining))
