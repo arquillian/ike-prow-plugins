387c4042e3f9dae88e47078cbea1232ed938feee
hemanik <hemanikatyal@gmail.com>
2018-06-05 16:25:05 +0530

    chore: remove tests with complete coverage in comment loader.

--- pkg/plugin/test-keeper/comment_message_test.go
+++ pkg/plugin/test-keeper/comment_message_test.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/arquillian/ike-prow-plugins/pkg/config"
 	. "github.com/arquillian/ike-prow-plugins/pkg/internal/test"
+	"github.com/arquillian/ike-prow-plugins/pkg/log"
 	"github.com/arquillian/ike-prow-plugins/pkg/plugin/test-keeper"
 	"github.com/arquillian/ike-prow-plugins/pkg/scm"
 	"github.com/microcosm-cc/bluemonday"
@@ -17,29 +18,15 @@ var _ = Describe("Test keeper comment message creation", func() {
 
 	Context("Creation of default comment messages that are sent to a validated PR when custom message file is not set", func() {
 
-		It("should create default message referencing to documentation when url to config is empty", func() {
+		It("should create default message referencing to test-keeper documentation when url to config is empty", func() {
 			// given
 			conf := testkeeper.PluginConfiguration{PluginConfiguration: config.PluginConfiguration{PluginName: testkeeper.ProwPluginName}}
 
 			// when
-			msg := testkeeper.CreateCommentMessage(conf, scm.RepositoryChange{})
+			msg := testkeeper.CreateCommentMessage(conf, scm.RepositoryChange{}, log.NewTestLogger())
 
 			// then
 			Expect(msg).To(ContainSubstring("http://arquillian.org/ike-prow-plugins/#_test_keeper_plugin"))
-			Expect(msg).To(ContainSubstring(testkeeper.BypassCheckComment))
-		})
-
-		It("should create default message referencing to config file when url to config is not empty", func() {
-			// given
-			url := "http://github.com/my/repo/test-keeper.yaml"
-			conf := testkeeper.PluginConfiguration{PluginConfiguration: config.PluginConfiguration{LocationURL: url}}
-
-			// when
-			msg := testkeeper.CreateCommentMessage(conf, scm.RepositoryChange{})
-
-			// then
-			Expect(msg).NotTo(ContainSubstring("http://arquillian.org/ike-prow-plugins/#_test_keeper_plugin"))
-			Expect(msg).To(ContainSubstring(url))
 			Expect(msg).To(ContainSubstring(testkeeper.BypassCheckComment))
 		})
 	})
@@ -50,38 +37,7 @@ var _ = Describe("Test keeper comment message creation", func() {
 			defer gock.OffAll()
 		})
 
-		It("should create message taken from a default hint file if plugin hint url is missing", func() {
-			// given
-			NonExistingRawGitHubFiles("test-keeper.yaml", "test-keeper.yml")
-
-			gock.New("https://raw.githubusercontent.com").
-				Get("owner/repo/46cb8fac44709e4ccaae97448c65e8f7320cfea7/.ike-prow/test-keeper_hint.md").
-				Reply(200).
-				BodyString("Custom message")
-
-			url := "http://github.com/my/repo/test-keeper.yaml"
-			conf := testkeeper.PluginConfiguration{
-				PluginConfiguration: config.PluginConfiguration{
-					LocationURL: url,
-					PluginName:  testkeeper.ProwPluginName,
-				},
-			}
-
-			change := scm.RepositoryChange{
-				Owner:    "owner",
-				RepoName: "repo",
-				Hash:     "46cb8fac44709e4ccaae97448c65e8f7320cfea7",
-			}
-
-			// when
-			msg := testkeeper.CreateCommentMessage(conf, change)
-			sanitizedMsg := removeHtmlElements(msg)
-
-			// then
-			Expect(sanitizedMsg).To(Equal("Custom message"))
-		})
-
-		It("should create message taken from a default hint file if location url is missing", func() {
+		It("should create message taken from a default test keeper hint file if location url is missing", func() {
 			// given
 			NonExistingRawGitHubFiles("test-keeper.yaml", "test-keeper.yml")
 
@@ -92,7 +48,7 @@ var _ = Describe("Test keeper comment message creation", func() {
 
 			conf := testkeeper.PluginConfiguration{
 				PluginConfiguration: config.PluginConfiguration{
-					PluginName:  testkeeper.ProwPluginName,
+					PluginName: testkeeper.ProwPluginName,
 				},
 			}
 
@@ -103,7 +59,7 @@ var _ = Describe("Test keeper comment message creation", func() {
 			}
 
 			// when
-			msg := testkeeper.CreateCommentMessage(conf, change)
+			msg := testkeeper.CreateCommentMessage(conf, change, log.NewTestLogger())
 			sanitizedMsg := removeHtmlElements(msg)
 
 			// then
@@ -118,38 +74,6 @@ var _ = Describe("Test keeper comment message creation", func() {
 		})
 
 		AfterEach(EnsureGockRequestsHaveBeenMatched)
-
-		It("should create message taken from a file set in config using relative path", func() {
-			// given
-			NonExistingRawGitHubFiles("test-keeper.yaml", "test-keeper.yml")
-
-			gock.New("https://raw.githubusercontent.com").
-				Get("owner/repo/46cb8fac44709e4ccaae97448c65e8f7320cfea7/path/to/test-keeper_hint.md").
-				Reply(200).
-				BodyString("Custom message")
-
-			url := "http://github.com/my/repo/test-keeper.yaml"
-			conf := testkeeper.PluginConfiguration{
-				PluginConfiguration: config.PluginConfiguration{
-					LocationURL: url,
-					PluginName:  testkeeper.ProwPluginName,
-					PluginHint:  "path/to/test-keeper_hint.md",
-				},
-			}
-
-			change := scm.RepositoryChange{
-				Owner:    "owner",
-				RepoName: "repo",
-				Hash:     "46cb8fac44709e4ccaae97448c65e8f7320cfea7",
-			}
-
-			// when
-			msg := testkeeper.CreateCommentMessage(conf, change)
-			sanitizedMsg := removeHtmlElements(msg)
-
-			// then
-			Expect(sanitizedMsg).To(Equal("Custom message"))
-		})
 
 		It("should create default message with no-found-custom-file suffix using wrong relative path", func() {
 			// given
@@ -171,7 +95,7 @@ var _ = Describe("Test keeper comment message creation", func() {
 			}
 
 			// when
-			msg := testkeeper.CreateCommentMessage(conf, change)
+			msg := testkeeper.CreateCommentMessage(conf, change, log.NewTestLogger())
 
 			// then
 			Expect(msg).NotTo(ContainSubstring("http://arquillian.org/ike-prow-plugins/#_test_keeper_plugin"))
@@ -180,132 +104,6 @@ var _ = Describe("Test keeper comment message creation", func() {
 			Expect(msg).To(ContainSubstring(
 				"https://raw.githubusercontent.com/owner/repo/46cb8fac44709e4ccaae97448c65e8f7320cfea7/" +
 					"path/to/test-keeper_hint.md"))
-		})
-
-		It("should create message taken from a file set in config using url", func() {
-			// given
-			gock.New("http://my.server.com").
-				Get("path/to/test-keeper_hint.md").Reply(200).
-				BodyString("Custom message")
-
-			url := "http://github.com/my/repo/test-keeper.yaml"
-			conf := testkeeper.PluginConfiguration{
-				PluginConfiguration: config.PluginConfiguration{
-					LocationURL: url,
-					PluginName:  testkeeper.ProwPluginName,
-					PluginHint:  "http://my.server.com/path/to/test-keeper_hint.md",
-				},
-			}
-
-			// when
-			msg := testkeeper.CreateCommentMessage(conf, scm.RepositoryChange{})
-			sanitizedMsg := removeHtmlElements(msg)
-
-			// then
-			Expect(sanitizedMsg).To(Equal("Custom message"))
-		})
-
-		It("should create message taken from a file with upper case filename set in config using url", func() {
-			// given
-			gock.New("http://my.server.com").
-				Get("path/to/TEST-KEEPER_HINT.MD").
-				Reply(200).
-				BodyString("Custom message")
-
-			url := "http://github.com/my/repo/test-keeper.yaml"
-			conf := testkeeper.PluginConfiguration{
-				PluginConfiguration: config.PluginConfiguration{
-					LocationURL: url,
-					PluginName:  testkeeper.ProwPluginName,
-					PluginHint:  "http://my.server.com/path/to/TEST-KEEPER_HINT.MD",
-				},
-			}
-
-			// when
-			msg := testkeeper.CreateCommentMessage(conf, scm.RepositoryChange{})
-			sanitizedMsg := removeHtmlElements(msg)
-
-			// then
-			Expect(sanitizedMsg).To(Equal("Custom message"))
-		})
-
-		It("should create default message with no-found-custom-file suffix using wrong url path", func() {
-			// given
-			gock.New("http://my.server.com").
-				Get("path/to/test-keeper_hint.md").
-				Reply(404)
-
-			url := "http://github.com/my/repo/test-keeper.yaml"
-			conf := testkeeper.PluginConfiguration{
-				PluginConfiguration: config.PluginConfiguration{
-					LocationURL: url,
-					PluginName:  testkeeper.ProwPluginName,
-					PluginHint:  "http://my.server.com/path/to/test-keeper_hint.md",
-				},
-			}
-
-			// when
-			msg := testkeeper.CreateCommentMessage(conf, scm.RepositoryChange{})
-
-			// then
-			Expect(msg).NotTo(ContainSubstring("http://arquillian.org/ike-prow-plugins/#_test_keeper_plugin"))
-			Expect(msg).To(ContainSubstring(url))
-			Expect(msg).To(ContainSubstring(testkeeper.BypassCheckComment))
-			Expect(msg).To(ContainSubstring(
-				"http://my.server.com/path/to/test-keeper_hint.md"))
-		})
-
-		It("should create default message with no-found-custom-file suffix using not-validate url", func() {
-			// given
-			NonExistingRawGitHubFiles("http/server.com/test-keeper_hint.md")
-
-			gock.New("https://raw.githubusercontent.com").
-				Get("owner/repo/46cb8fac44709e4ccaae97448c65e8f7320cfea7/path/to/test-keeper_hint.md").
-				Reply(404)
-
-			url := "http://github.com/my/repo/test-keeper.yaml"
-			conf := testkeeper.PluginConfiguration{
-				PluginConfiguration: config.PluginConfiguration{
-					LocationURL: url,
-					PluginName:  testkeeper.ProwPluginName,
-					PluginHint:  "http/server.com/test-keeper_hint.md",
-				},
-			}
-
-			change := scm.RepositoryChange{
-				Owner:    "owner",
-				RepoName: "repo",
-				Hash:     "46cb8fac44709e4ccaae97448c65e8f7320cfea7",
-			}
-
-			// when
-			msg := testkeeper.CreateCommentMessage(conf, change)
-
-			// then
-			Expect(msg).NotTo(ContainSubstring("http://arquillian.org/ike-prow-plugins/#_test_keeper_plugin"))
-			Expect(msg).To(ContainSubstring(url))
-			Expect(msg).To(ContainSubstring(testkeeper.BypassCheckComment))
-			Expect(msg).To(ContainSubstring(
-				"https://raw.githubusercontent.com/owner/repo/46cb8fac44709e4ccaae97448c65e8f7320cfea7/" +
-					"http/server.com/test-keeper_hint.md"))
-		})
-
-		It("should create message taken from a string set in config", func() {
-			// given
-			url := "http://github.com/my/repo/test-keeper.yaml"
-			conf := testkeeper.PluginConfiguration{
-				PluginConfiguration: config.PluginConfiguration{
-					LocationURL: url,
-					PluginName:  testkeeper.ProwPluginName,
-					PluginHint:  "Custom message",
-				},
-			}
-
-			// when
-			msg := testkeeper.CreateCommentMessage(conf, scm.RepositoryChange{})
-
-			// then
-			Expect(msg).To(Equal("Custom message"))
 		})
 
 		It("should create message with file path as content using invalid filename pattern", func() {
@@ -324,7 +122,7 @@ var _ = Describe("Test keeper comment message creation", func() {
 			}
 
 			// when
-			msg := testkeeper.CreateCommentMessage(conf, scm.RepositoryChange{})
+			msg := testkeeper.CreateCommentMessage(conf, scm.RepositoryChange{}, log.NewTestLogger())
 
 			// then
 			Expect(msg).NotTo(ContainSubstring("http://arquillian.org/ike-prow-plugins/#_test_keeper_plugin"))
