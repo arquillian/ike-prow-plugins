e7fdb39b9db709d948f2bb616eded481c88ec78d
hemanik <hemanikatyal@gmail.com>
2018-06-05 14:11:20 +0530

    test: add test for comment message loader.

---
+++ pkg/comment/comment_suite_test.go
@@ -0,0 +1,14 @@
+package comment_test
+
+import (
+	"testing"
+
+	. "github.com/arquillian/ike-prow-plugins/pkg/internal/test"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+func TestConfig(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecWithJUnitReporter(t, "Plugin Comment Loader Suite")
+}
