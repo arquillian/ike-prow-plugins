7ee236abc913173cb39643763048ae8d876b1cd0
Matous Jobanek <mjobanek@redhat.com>
2018-07-13 16:37:44 +0200

    add links to semantic commit message blog post

--- pkg/plugin/pr-sanitizer/checks.go
+++ pkg/plugin/pr-sanitizer/checks.go
@@ -20,7 +20,7 @@ var (
 
 const (
 	// TitleFailureMessage is a message used in GH Status as description when the PR title does not follow semantic message style
-	TitleFailureMessage = "#### Semantic title\nThe PR title `%s` does not conform with the semantic message style. " +
+	TitleFailureMessage = "#### Semantic title\nThe PR title `%s` does not conform with the [semantic message](https://seesparkbox.com/foundry/semantic_commit_messages) style. " +
 		"The semantic message makes your changelog and git history clean. " +
 		"Please, edit the PR by prefixing it with one of the type prefixes that are valid for your repository: %s."
 
